1c46cac53f62c6d544999ccfb66fbef4c68a10fc
Francesco Lobbia <142682839+FraLobbia@users.noreply.github.com>
2024-02-01 15:15:47 +0100

    Aggiungere i file di progetto.

---
+++ Utente/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utente
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("1) Registrati \n2) Effetua il login");
+            Console.WriteLine("Scegli un'opzione: ");
+            int resp = int.Parse(Console.ReadLine());
+            Utente utente = new Utente();
+            Console.WriteLine("Inserisci email: ");
+            string email = Console.ReadLine();
+            utente.SetEmail(email);
+            Console.WriteLine("Inserisci password: ");
+            string password = Console.ReadLine();
+            utente.SetPassword(password);
+
+            do
+            {
+                Console.WriteLine("Effetua il login: ");
+                Console.WriteLine("Email: ");
+                email = Console.ReadLine();
+                Console.WriteLine("Password: ");
+                password = Console.ReadLine();
+                Console.WriteLine(utente.Login(email, password));
+            } while {!utente.getIsLogged()};
+
+
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Utente/Utente.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Utente
+{
+    internal class Utente
+    {
+        private string email;
+        private string password;
+        private bool isLoggedIn;
+        private int[] voti {  get; set; }
+
+        public Utente() { }
+        public Utente(string email, string password)
+        {
+            this.email = email;
+            this.password = password;
+            this.isLoggedIn = false;
+            this.voti = new int[] { 0, 0, 0};
+        }
+
+        public void SetEmail(string email)
+        {
+            this.email = email;
+        }
+        public void SetPassword(string password)
+        {
+            this.password = password;
+        }
+
+        public bool getIsLogged()
+        {
+            return this.isLoggedIn;
+        }
+
+        public string Login(string email, string password) 
+        {
+            if (this.email == email && this.password == password)
+            {
+                isLoggedIn = true;
+                return "Login effettuato con successo!";
+            }
+            else
+            {
+                return "Email e password non corretti";
+            }
+        }
+
+        public void Vota()
+        {
+            for (int i = 0; i < voti.Length; i++)
+            {
+                Console.Write($"Domanda {i + 1}");
+                Console.Write("Opzione 1");
+                Console.Write("Opzione 2");
+                Console.Write("Opzione 3");
+                voti[i] = int.Parse(Console.ReadLine());
+            }
+        }
+    }
+}
